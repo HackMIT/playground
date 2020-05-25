e2dfc6df5894535395759b0e8ad66f9fa5e49e20
Jack Cook <jbcook418@gmail.com>
2020-05-25 13:50:49 -0400

    Communicate with frontend repo

--- client.go
+++ client.go
@@ -127,6 +127,11 @@ func (c *Client) writePump() {
 
 // serveWs handles websocket requests from the peer.
 func serveWs(hub *Hub, w http.ResponseWriter, r *http.Request) {
+	// TODO: Create more strict origin checks -- this is a security risk
+	upgrader.CheckOrigin = func(r *http.Request) bool {
+		return true
+	}
+
 	conn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		log.Println(err)
--- main.go
+++ main.go
@@ -10,26 +10,6 @@ import (
 
 var addr = flag.String("addr", ":8080", "http service address")
 
-func serveHome(w http.ResponseWriter, r *http.Request) {
-	log.Println(r.URL)
-	// if r.URL.Path != "/" {
-	// 	http.Error(w, "Not found", http.StatusNotFound)
-	// 	return
-	// }
-	if r.Method != "GET" {
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-		return
-	}
-
-	path := r.URL.String()[1:]
-
-	if (path == "") {
-		path = "home.html"
-	}
-
-	http.ServeFile(w, r, path)
-}
-
 func main() {
 	flag.Parse()
 	db := redis.NewClient(&redis.Options{
@@ -49,7 +29,6 @@ func main() {
 	hub := newHub()
 	world := newWorld()
 	go hub.run(world)
-	http.HandleFunc("/", serveHome)
 	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
 		serveWs(hub, w, r)
 	})
