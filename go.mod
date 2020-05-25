5417ef946eef4159c26f72eb07ff22f40af978de
Jack Cook <jbcook418@gmail.com>
2020-05-25 10:33:07 -0400

    Added assets

--- go.mod
+++ go.mod
@@ -1,8 +1,9 @@
 module github.com/techx/playground
 
 require (
-    github.com/google/uuid v1.1.1
-    github.com/gorilla/websocket v1.4.2
+	github.com/go-redis/redis/v7 v7.2.0
+	github.com/google/uuid v1.1.1
+	github.com/gorilla/websocket v1.4.2
 )
 
 go 1.14
--- main.go
+++ main.go
@@ -4,25 +4,48 @@ import (
 	"flag"
 	"log"
 	"net/http"
+
+	"github.com/go-redis/redis/v7"
 )
 
 var addr = flag.String("addr", ":8080", "http service address")
 
 func serveHome(w http.ResponseWriter, r *http.Request) {
 	log.Println(r.URL)
-	if r.URL.Path != "/" {
-		http.Error(w, "Not found", http.StatusNotFound)
-		return
-	}
+	// if r.URL.Path != "/" {
+	// 	http.Error(w, "Not found", http.StatusNotFound)
+	// 	return
+	// }
 	if r.Method != "GET" {
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 		return
 	}
-	http.ServeFile(w, r, "home.html")
+
+	path := r.URL.String()[1:]
+
+	if (path == "") {
+		path = "home.html"
+	}
+
+	http.ServeFile(w, r, path)
 }
 
 func main() {
 	flag.Parse()
+	db := redis.NewClient(&redis.Options{
+		Addr: "localhost:6379",
+		Password: "",
+		DB: 0,
+	})
+	write_err := db.Set("key", "value", 0).Err()
+
+	if write_err != nil {
+		panic(write_err)
+	}
+
+	val, _ := db.Get("key").Result()
+	log.Println(val)
+
 	hub := newHub()
 	world := newWorld()
 	go hub.run(world)
