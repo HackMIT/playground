4da439962d16982a74977e43421adc3f6c6fcf95
Julia Gonik <jgonik@mit.edu>
2020-09-17 13:07:48 -0700

    Uncomment jukebox timer and decrease it to 30 seconds

--- socket/hub.go
+++ socket/hub.go
@@ -864,26 +864,26 @@ func (h *Hub) processMessage(m *SocketMessage) {
 			return
 		}
 
-		// var jukeboxTimestamp time.Time
+		var jukeboxTimestamp time.Time
 		jukeboxQuery := "character:" + m.sender.character.ID + ":jukeboxTimestamp"
-		// jukeboxKeyExists, _ := db.GetInstance().Exists(jukeboxQuery).Result()
-		// // User has never added a song to queue
-		// if (jukeboxKeyExists != 1) {
-		// 	jukeboxTimestamp = time.Now()
-		// 	res.RequiresWarning = true
-		// } else {
-		// 	timestampString, _ := db.GetInstance().Get(jukeboxQuery).Result()
-		// 	var _ error
-		// 	jukeboxTimestamp, _ = time.Parse(time.RFC3339, timestampString)
-		// }
+		jukeboxKeyExists, _ := db.GetInstance().Exists(jukeboxQuery).Result()
+		// User has never added a song to queue
+		if (jukeboxKeyExists != 1) {
+			jukeboxTimestamp = time.Now()
+			res.RequiresWarning = true
+		} else {
+			timestampString, _ := db.GetInstance().Get(jukeboxQuery).Result()
+			var _ error
+			jukeboxTimestamp, _ = time.Parse(time.RFC3339, timestampString)
+		}
 
 		// 15 minutes has not yet passed since user last submitted a song
-		// if jukeboxTimestamp.After(time.Now()) {
-		// 	errorPacket := packet.NewErrorPacket(401)
-		// 	data, _ := json.Marshal(errorPacket)
-		// 	m.sender.send <- data
-		// 	return
-		// }
+		if jukeboxTimestamp.After(time.Now()) {
+			errorPacket := packet.NewErrorPacket(401)
+			data, _ := json.Marshal(errorPacket)
+			m.sender.send <- data
+			return
+		}
 
 		// Make the YouTube API call
 		fmt.Println(config.GetSecret(config.YouTubeKey))
@@ -942,7 +942,7 @@ func (h *Hub) processMessage(m *SocketMessage) {
 		songID := uuid.New().String()
 		res.ID = songID
 
-		jukeboxTime := time.Now().Add(time.Minute * 15)
+		jukeboxTime := time.Now().Add(time.Second * 30)
 
 		pip := db.GetInstance().Pipeline()
 		pip.HSet("song:"+songID, utils.StructToMap(p.Song))
