8e89ca87072ae8da978645db723118c4c8326a04
Jack Cook <hello@jackcook.com>
2020-09-19 03:39:33 -0400

    Increase maximum packet size

--- socket/client.go
+++ socket/client.go
@@ -28,7 +28,7 @@ func NewClient(hub *Hub, conn *websocket.Conn) *Client {
 	c := new(Client)
 	c.hub = hub
 	c.conn = conn
-	c.send = make(chan []byte, 1024)
+	c.send = make(chan []byte, 4096)
 	c.id = uuid.New().String()
 	return c
 }
